51faf49fb0b4731067d93d1beef64dcab954d297
andresturu <andresturu@icloud.com>
2025-07-05 13:54:37 -0700

    dfdf

---
+++ Part_1.py
@@ -0,0 +1,14 @@
+class JazzMusician:
+    def __init__(self, first, last, nickname, instrument):
+        self.fname = first #normll would just do self.first = first but I'm just seeing if this works too
+        self.lname = last #normally would just do self.last = last but I'm just seeing if this works
+        self.nname = nickname
+        self.instrument = instrument
+    
+    def player_introduction(self):
+        return '{} "{}" {}'.format(self.fname, self.nname, self.lname)
+
+dude_1 = JazzMusician('Greg', 'Heffley', 'The heffler', 'Drums')
+
+print(dude_1.player_introduction())
+print(JazzMusician.player_introduction(dude_1))
